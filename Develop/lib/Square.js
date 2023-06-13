12de46260ff93570c330601ad034c54427c39d39
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-13 18:43:10 -0400

    square file

---
+++ Develop/lib/Square.js
@@ -0,0 +1,20 @@
+const Shape = require('./shape.js')
+
+// extend parent to child
+class Square extends Shape {
+    constructor(shapeColor, text, textColor) {
+        super(shapeColor, text, textColor);
+    };
+    render() {
+        return `
+        <svg version="1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+        <rect width="200" height="200" fill="${this.shapeColor}" />
+        <text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+        </svg>
+        `
+    };
+};
+
+module.exports = Square;
