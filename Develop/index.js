febc7555bf58cd1b9bd481882e4d179378cad046
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-13 18:06:10 -0400

    added init function to index file

--- Develop/index.js
+++ Develop/index.js
@@ -1,2 +1,22 @@
+// DEPENDENCIES
 const inquirer = require('inquirer');
-const questions = 
+const questions = require('./lib/questions.js');
+
+
+// need a function to create new shape
+
+
+
+// INITIALIZATION 
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            logo(response);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
+
+init();
